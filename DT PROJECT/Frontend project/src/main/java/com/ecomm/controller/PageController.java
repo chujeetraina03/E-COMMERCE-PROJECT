8d2249f7c6419d8b27ee3df7dd0c72d3dfb1826d
chujeetraina03 <48875314+chujeetraina03@users.noreply.github.com>
2019-10-10 14:49:49 +0530

    Add files via upload

---
+++ DT PROJECT/Frontend project/src/main/java/com/ecomm/controller/PageController.java
@@ -0,0 +1,26 @@
+package com.ecomm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController 
+{
+	@RequestMapping("/login")
+	public String showLoginPage()
+	{
+		return "Login";
+	}
+	
+	@RequestMapping("/register")
+	public String showRegisterPage()
+	{
+		return "Register";
+	}
+	
+	@RequestMapping("/contactUs")
+	public String showContactUsPage()
+	{
+		return "ContactUs";
+	}
+}
---
+++ DT PROJECT/Frontend project/src/main/webapp/WEB-INF/views/ContactUs.jsp
@@ -0,0 +1,6 @@
+<%@include file="CommonHeader.jsp" %>
+
+<h3 align="center">ContactUs Page</h3>
+
+</body>
+</html>
